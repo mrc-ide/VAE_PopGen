4801f1e7c90ad9ecd934ffa7bd8d7221dba4511d
elizavetasemenova <elizaveta.p.semenova@gmail.com>
2024-06-28 15:58:26 +0100

    update script to generate data

--- R_sim_lattice/example_lattice.R
+++ R_sim_lattice/example_lattice.R
@@ -16,8 +16,8 @@ source("src/functions.R")
 demes_x <- 32
 demes_y <- 32
 N <- 1e3
-mu <- 1e-4
-m <- 1e-2
+mu <- 1e-7
+m <- 5*1e-3
 t_out <- seq(0, 5e4, l = 6)
 
 
@@ -39,3 +39,38 @@ df_mean %>%
 
 # plot matrices to visualise spatial autocorrelation
 plot_lattice_mat(sim1)
+
+
+n_sims <- 100000
+
+for (i in 1:n_sims){
+  print(i)
+  
+  N_df  <- rep(N, length(t_out))
+  mu_df <- rep(mu, length(t_out))
+  m_df  <- rep(m, length(t_out))
+  
+  df_params <- data.frame(cbind(N_df, mu_df, m_df))
+  names(df_params) <- c('N', 'mu', 'm')
+  
+  # simulate from lattice model
+  sim1 <- sim_lattice_biallelic(demes_x = demes_x, demes_y = demes_y,
+                                N = N, mu = mu, m = m, t_out = t_out,
+                                torus = TRUE)
+  
+  df_sim <- data.frame(matrix(unlist(sim1), nrow=length(sim1), byrow=TRUE))
+  names(df_sim) <- paste0('p', c(1: (demes_x * demes_y)))
+  
+  df_sim <- cbind(df_params, df_sim)
+  
+  if (i==1){
+    df <- df_sim
+  } else {
+    df <- rbind(df, df_sim)
+  }
+}
+
+fname <- paste0("/home/scratch/popgen/samples/samps_N",  N, "_mu", mu, "_m", m, ".csv")
+write.csv(df, fname, row.names=FALSE)
+
+
